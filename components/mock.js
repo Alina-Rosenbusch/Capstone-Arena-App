f048b3be475777284aa0ee40ed68f32d625fbaea
Alina Rosenbusch <alina.rosenbusch@gmx.de>
2023-07-20 11:11:49 +0200

    add dummy data

---
+++ components/mock.js
@@ -0,0 +1,34 @@
+trainingData = [
+  {
+    bookedArena: "Dressage Arena",
+    bookedPerson: "Ally",
+    time: "17:00-18:00",
+    date: "20.July.2023",
+    singleRider: false,
+    title: "Training",
+  },
+  {
+    bookedArena: "Dressage Arena",
+    bookedPerson: "Jelena",
+    time: "15:00-16:00",
+    date: "20.July.2023",
+    singleRider: true,
+    title: "Lesson with Angelo",
+  },
+  {
+    bookedArena: "Jumping Arena",
+    bookedPerson: "Flo",
+    time: "17:00-18:00",
+    date: "21.July.2023",
+    singleRider: false,
+    title: "Training",
+  },
+  {
+    bookedArena: "Round Pen",
+    bookedPerson: "James",
+    time: "12:00-13:30",
+    date: "22.July.2023",
+    singleRider: true,
+    title: "Parelli Session Level 2 Training",
+  },
+];
