2ca07b20fa1dfb6ad187995d89ca949a808a2bfa
Alina Rosenbusch <alina.rosenbusch@gmx.de>
2023-07-28 12:24:10 +0200

    cleaning code

--- pages/api/bookings/index.js
+++ pages/api/bookings/index.js
@@ -3,11 +3,10 @@ import Booking from "../../../db/models/bookings";
 
 export default async function bookingHandler(request, response) {
   await dbConnect();
-console.log(request)
+
   if (request.method === "GET") {
     const bookings = await Booking.find();
 
-    console.log(Booking)
     return response.status(200).json(bookings);
   } else {
     return response.status(404).json({ status: "Not Found" });
